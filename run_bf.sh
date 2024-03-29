37e863f14205e4fd29834b37c850caac4ca8451b
mingu6 <mxu925@gmail.com>
2024-03-30 09:06:28 +1100

    updated final hparams

--- run_bf.sh
+++ run_bf.sh
@@ -6,7 +6,5 @@ seed=0
 gpu=0
 
 for i in ${!actions[@]}; do
-	# python3 train.py -d Breakfast -ac ${actions[$i]} -c ${clusters[$i]} -ne 15 -g $gpu --seed 0 -s --rho 0.2 -lat 0.09 -r 0.04 -lr 1e-3 -wd 1e-4 -vf 5 --group improved_solver --wandb -v -ua
-	python3 train.py -d Breakfast -ac ${actions[$i]} -c ${clusters[$i]} -ne 15 -g $gpu --seed 0 -s --rho 0.2 -lat 0.1 -r 0.04 -ae 0.7 -at 0.4 -lr 1e-3 -wd 1e-4 -vf 5 --group improved_solver --wandb -v -ua
-	# python3 train.py -d Breakfast -ac ${actions[$i]} -c ${clusters[$i]} -ne 15 -g $gpu --seed 0 -s --rho 0.15 -ut 0.05 -at 0.4 -ae 0.6 -r 0.04 -lr 1e-3 -wd 1e-4 -vf 5 --group improved_solver --wandb -v
+	python3 train.py -d Breakfast -ac ${actions[$i]} -c ${clusters[$i]} -ne 15 -g $gpu --seed 0 -s --rho 0.2 -lat 0.1 -r 0.04 -ae 0.7 -at 0.4 -lr 1e-3 -wd 1e-4 -vf 5 --group main_results --wandb -v -ua
 done
--- run_sensitivity.sh
+++ run_sensitivity.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 # params=(0.02 0.04 0.06 0.08 0.1) # gw radius
-# params=(0.0 0.03 0.06 0.09 0.12) # unbalanced weight
+# params=(0.08 0.11 0.14 0.17 0.2) # unbalanced weight
 # params=(0.1 0.3 0.5 0.7 0.9) # alpha
-# params=(0.1 0.2 0.3 0.4 0.5) # rho
-# params=(0.02 0.04 0.06 0.08 0.1) # training eps
-params=(11 17 22 28 33) # number of classes
-seed=0
-gpu=0
+params=(0.1 0.15 0.2 0.25 0.3) # rho
+# params=(0.03 0.05 0.07 0.09 0.11) # training eps
+# params=(10 14 19 24 29) # number of classes
 
 for i in ${!params[@]}; do
-	# python3 train.py -d FSeval -ac all -c ${params[$i]} -ne 20 -g 1 --seed 0 --group sens_K --rho 0.1 -ut 0.06 -at 0.3 -vf 5 -lr 1e-3 -wd 1e-4 -r 0.04 --wandb
-	# python3 train.py -d FS -ac all -c ${params[$i]} -ne 20 -g 0 --seed 0 --group sens_K --rho 0.2 -ut 0.09 -et 0.06 -ee 0.01 -at 0.3 -vf 5 -lr 1e-3 -wd 1e-4 -r 0.04 --wandb
-	python3 train.py -d desktop_assembly -ac all -c ${params[$i]} -ne 30 -g 1 --seed 0 --group sens_K --rho 0.3 -ut 0.09 -at 0.3 -vf 5 -lr 1e-3 -wd 1e-4 -r 0.02 --wandb -ls 512 128 40
+	# python3 train.py -d FSeval -ac all -c 12 -ne 30 -g 1 --seed 0 --group sens_eps --rho 0.15 -lat 0.11 -et ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ua
+	# python3 train.py -d FS -ac all -c 19 -ne 30 -g 1 --seed 0 --group sens_eps --rho 0.15 -lat 0.15 -et ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ua
+	# python3 train.py -d desktop_assembly -ac all -c 22 -ne 30 -g 1 --seed 0 --group sens_eps --rho 0.25 -lat 0.16 -et ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ls 512 128 40 -ua -r 0.02
+	# python3 train.py -d FSeval -ac all -c 12 -ne 30 -g 1 --seed 0 --group sens_lambda --rho 0.15 -lat ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ua
+	# python3 train.py -d FS -ac all -c 19 -ne 30 -g 1 --seed 0 --group sens_lambda --rho 0.15 -lat ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ua
+	# python3 train.py -d desktop_assembly -ac all -c 22 -ne 30 -g 1 --seed 0 --group sens_lambda --rho 0.25 -lat ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ls 512 128 40 -ua -r 0.02
+	python3 train.py -d FSeval -ac all -c 12 -ne 30 -g 1 --seed 0 --group sens_rho -lat 0.11 --rho ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ua
+	python3 train.py -d FS -ac all -c 19 -ne 30 -g 1 --seed 0 --group sens_rho -lat 0.15 --rho ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ua
+	python3 train.py -d desktop_assembly -ac all -c 22 -ne 30 -g 1 --seed 0 --group sens_rho -lat 0.16 --rho ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ls 512 128 40 -ua -r 0.02
+	# python3 train.py -d FSeval -ac all -c 12 -ne 30 -g 1 --seed 0 --group sens_radius --rho 0.15 -lat 0.11 -r ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ua
+	# python3 train.py -d FS -ac all -c 19 -ne 30 -g 1 --seed 0 --group sens_radius --rho 0.15 -lat 0.15 -r ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ua
+	# python3 train.py -d desktop_assembly -ac all -c 22 -ne 30 -g 1 --seed 0 --group sens_radius --rho 0.25 -lat 0.16 -r ${params[$i]} -vf 5 -lr 1e-3 -wd 1e-4 --wandb -ls 512 128 40 -ua
 done
 
